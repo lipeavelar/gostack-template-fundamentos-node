542b902cf06bc520b82ae9beb8196539ab9465e5
Felipe Avelar <lipeavelar@gmail.com>
2020-05-24 22:44:52 -0300

    Solution
    Signed-off-by: Felipe Avelar <lipeavelar@gmail.com>

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,30 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach(transaction => {
+      balance[transaction.type] += transaction.value;
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(transaction: Transaction): Transaction {
+    // create new transaction
+    const newTransaction = new Transaction(transaction);
+    this.transactions.push(newTransaction);
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(transaction: Transaction): Transaction {
+    if (
+      transaction.type === 'outcome' &&
+      transaction.value > this.transactionsRepository.getBalance().total
+    )
+      throw new Error('Outcome cannot overcome balance');
+
+    return this.transactionsRepository.create(transaction);
   }
 }
 
